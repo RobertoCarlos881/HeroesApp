ebf1f03a1e7ef5a353406c3408d0d0b974649f35
RobertoCarlos881 <roberto881carlos881@hotmail.com>
2023-06-04 15:02:15 -0600

    applied reactiveforms

--- src/app/heroes/pages/new-pages/new-pages.component.ts
+++ src/app/heroes/pages/new-pages/new-pages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-pages',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
   ]
 })
 export class NewPagesComponent {
+
+  public heroForm = new FormControl({
+    id: new FormControl(''),
+    superhero: new FormControl<string>('', {nonNullable: true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl('')
+  })
+
   public publishers =  [
     {id: 'DC Comics', desc: 'DC - Comics'},
     {id: 'Marvel Comics', desc: 'Marvel - Comics'}
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, of, map } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
 @Injectable({providedIn: 'root'})
 export class HeroesService {
   private baseUrl: string = environments.baseUrl;
+
   constructor(private http: HttpClient) { }
 
-  getHeroes():Observable<Hero[]> {
-    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
-  getHeroById(id: string):Observable<Hero|undefined> {
+  getHeroById(id: string): Observable<Hero|undefined> {
     return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
       .pipe(
         catchError(error => of(undefined))
       );
   }
 
-  getSuggestions(query: string):Observable<Hero[]> {
-    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`)
+  getSuggestions(query: string): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updatedHero(hero: Hero): Observable<Hero> {
+    if(!hero.id) throw Error('Hero id is required');
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHero(id: string): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(err => of(false)),
+        map(resp => true)
+      );
   }
 }
